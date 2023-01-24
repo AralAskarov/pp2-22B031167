13527ffca63f3b442d3518bfffd91b6d872d9369
unknown <askarov.10.09@gmail.com>
2023-01-24 07:36:44 +0600

    Python Data Types

---
+++ TSIS1/Python_DataTypes.py
@@ -0,0 +1,22 @@
+# Типы данных, переменных
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+x = 1j	#complex
+print(x)
+
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple
+
+x = {"name" : "John", "age" : 36}	#dict
+print(x)
+
+
