b983a3c93dc640b495c3210b9ea00cb6b3c7183e
unknown <askarov.10.09@gmail.com>
2023-01-24 07:35:01 +0600

    Python Casting

---
+++ TSIS1/Python_Casting.py
@@ -0,0 +1,39 @@
+a = int(1)   # x will be 1
+print(a)
+print(type(a))
+
+b = int(2.8) # y will be 2
+print(b)
+print(type(b))
+
+c = int("3") # z will be 3
+print(c)
+print(type(c))
+
+d = float(1)     # x will be 1.0
+print(d)
+print(type(d))
+
+e = float(2.8)   # y will be 2.8
+print(e)
+print(type(e))
+
+f = float("3")   # z will be 3.0
+print(f)
+print(type(f))
+
+g = float("4.2") # w will be 4.2
+print(g)
+print(type(g))
+
+i = str("s1") # x will be 's1'
+print(i)
+print(type(i))
+
+j = str(2)    # y will be '2'
+print(j)
+print(type(j))
+
+k = str(3.0)  # z will be '3.0'
+print(k)
+print(type(k))
