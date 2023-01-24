92ada28e4f41a3b045bf5f8992d6df81bc213ffb
unknown <askarov.10.09@gmail.com>
2023-01-24 07:37:30 +0600

    Python Getting Started

---
+++ TSIS1/Python_GetStarted.py
@@ -0,0 +1,8 @@
+# python --version -узнать версию питона
+# C:\Users\Admin\Documents\pp2>python --version
+# Python 3.11.1
+
+# python hello.py - запуск кода в терминале
+print('Hello, World!!')
+# C:\Users\Admin\Documents\pp2\pp2-22B031167\TSIS1>python PythonGetStarted.py
+# Hello, World!!
