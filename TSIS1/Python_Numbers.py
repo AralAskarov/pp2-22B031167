4c48dd61da2df9c6f59de0501d2f45d7bc2af6c2
unknown <askarov.10.09@gmail.com>
2023-01-24 07:40:43 +0600

    Python Numbers

---
+++ TSIS1/Python_Numbers.py
@@ -0,0 +1,53 @@
+# There are three numeric types in Python:
+
+# int
+# float
+# complex
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# INT
+# у инта нет ограниченний по размеру 
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# FLOAT
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Число с плавающей запятой также может быть научным числом с буквой «е», обозначающей степень 10.
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(x,y,z)
+
+# COMPLEX
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
