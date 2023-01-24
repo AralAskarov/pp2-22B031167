1fa3845ca9a38b0996af72460bf43f0c0037deb2
unknown <askarov.10.09@gmail.com>
2023-01-24 07:46:43 +0600

    Python Syntax

---
+++ TSIS1/Python_Syntax.py
@@ -0,0 +1,18 @@
+# Indentation in python
+if 5 > 2:
+    print('Five is bigger than two!')
+#нужно делать отступ в 4 пробела
+
+# if 5 > 2:
+# print("Five is greater than two!")  Syntax error
+
+#должен быть отступ в 4 пробела, но можно хотя бы 1
+print('\n')
+print('Отступ в 1 пробел:')
+if 5>2:
+ print('Five is bigger than two!')
+
+# Если начали писать отступом в 1 или 4 пробела, то дальше нужно делать такой же отступ
+# if 5 > 2:
+#     print('Five is bigger than two!')
+#  print('Five is bigger than two!')   SYNTAX ERROR
